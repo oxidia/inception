f6e881ac1c9feb74e7ac502188479333a3525915
Youssef Bahlaoui <youssef-bahlaoui@outlook.fr>
2023-04-09 22:03:31 +0000

    feat: create mariadb service

---
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ -z $WP_DB_NAME ] || [ -z $WP_DB_USER ] || [ -z $WP_DB_PASSWORD ]; then
+    echo "environment variables WP_DB_NAME, WP_DB_USER and WP_DB_PASSWORD are required."
+    exit 1
+fi
+
+# https://mariadb.com/kb/en/mysql_install_db/
+
+mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+
+# https://sebhastian.com/mysql-skip-grant-tables-error-fix/
+
+mariadbd --user=mysql --datadir=/var/lib/mysql --bootstrap << EOF
+FLUSH PRIVILEGES;
+
+CREATE DATABASE ${WP_DB_NAME};
+
+CREATE USER '${WP_DB_USER}' IDENTIFIED BY '${WP_DB_PASSWORD}';
+
+GRANT ALL PRIVILEGES ON *.* TO '${WP_DB_USER}';
+
+EOF
+
+mariadbd-safe --datadir="/var/lib/mysql"
